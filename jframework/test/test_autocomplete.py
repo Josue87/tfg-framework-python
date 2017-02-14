b0647589125e80986201b304f9075a469dfafe93
Josue <j.encinar.87@gmail.com>
2017-02-14 18:34:48 +0100

    actualizado test

--- jframework/test/test_autocomplete.py
+++ jframework/test/test_autocomplete.py
@@ -1,8 +1,7 @@
 import unittest
-import unittest.mock as mock
 import os
 import pexpect
-from jframework.extras.autocomplete import MyCompleter
+
 
 
 class Autocomolete(unittest.TestCase):
@@ -11,11 +10,8 @@ class Autocomolete(unittest.TestCase):
         self.path = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir,  os.pardir,'jf.py'))
         self.shell = pexpect.spawn('python3 {}'.format(self.path))
         self.shell.send('\r\n')
-        self.myCompleter = MyCompleter(["exit", "load", "modules", "show_sessions",
-                                      "session", "delete_session", "credentials"], mock.MagicMock())
 
     def tearDown(self):
-        self.myCompleter = None
         self.shell = None
 
     def test_prompt(self):
@@ -29,11 +25,12 @@ class Autocomolete(unittest.TestCase):
     def test_tabulator(self):
         self.shell.send("\t\t")
         self.shell.expect("credentials", timeout=1)
-        self.shell.expect("modules", timeout=1)
-        self.shell.expect("show_sessions", timeout=1)
-        self.shell.expect("delete_session", timeout=1)
+        self.shell.expect("exit", timeout=1)
         self.shell.expect("load", timeout=1)
         self.shell.expect("session", timeout=1)
+        self.shell.expect("delete_session", timeout=1)
+        self.shell.expect("show_sessions", timeout=1)
+
 
 if __name__ == "__main__":
     unittest.main()
--- jframework/test/test_options.py
+++ jframework/test/test_options.py
@@ -1,4 +1,4 @@
-import unittest, sys
+import unittest
 import unittest.mock as mock
 import jframework.shell as sh
 
@@ -9,19 +9,17 @@ class TestOptions(unittest.TestCase):
         sh.Shell.initial = mock.Mock()
         self.shell = sh.Shell()
         self.shell.myModule = mock.MagicMock()
-        self.shell.completer = mock.MagicMock()
 
     def test_exec_command(self):
         host = '127.0.0.1'
         new_host = "192.168.0.1"
-
+        self.shell.initial()
         self.shell.myModule.get_options = ['ip']
-        self.shell.myModule.HOST = host
-        self.assertEqual(self.shell.myModule.HOST, host)
-
+        self.shell.myModule.host = host
+        self.assertEqual(self.shell.myModule.host, host)
         self.shell.myModule.ip = (lambda host: set_ip(self.shell.myModule, host))
-        self.shell.exec_command("put ip {}".format(new_host))
-        self.assertEqual(self.shell.myModule.HOST, new_host)
+        self.shell.exec_command(("put ip {}".format(new_host)).split(" "))
+        self.assertEqual(self.shell.myModule.host, new_host)
 
 
     def test_exec_commad2(self):
@@ -30,7 +28,8 @@ class TestOptions(unittest.TestCase):
 
 
 def set_ip(shell, host):
-    shell.HOST = host
+    print(".")
+    shell.host = host
 
 if __name__ == "__main__":
     unittest.main()
